6e9bdfbf86ce2bf665fd9ce39f5d5b905bafef0f
Muhammad Andhika Gema Putra <andhika_gemap@apps.ipb.ac.id>
2021-01-05 13:17:00 +0700

    Add LoadingScreen, Add MainMenu BGM

---
+++ Tugas Akhir - Copy/Assets/GameLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameLoader : MonoBehaviour
+{
+    public GameObject GameLoad;
+    public Slider slider;
+    public void LoadGame (int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        GameLoad.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress/.9f);
+            Debug.Log(slider.value = progress);
+            yield return null;
+        }
+    }
+}
